8c239d470acfaa0554171da56d45476e29486d5e
Andres Alvarez <72172628+andresalvarezb@users.noreply.github.com>
2024-03-20 16:30:27 -0500

    fix: :sparkles: delete js bind and create each view with html and add missing styles

--- src/js/index.js
+++ src/js/index.js
@@ -1,53 +1,61 @@
-import { DB } from "../data/db.js";
-import { abrigosContent } from "./components/abrigos.js";
-import { camisetasContent } from "./components/camisetas.js";
-import { cardProduct } from "./components/card.js";
-import { pantalonesContent } from "./components/pantalones.js";
+// import { DB } from "../data/db.js";
+// import { abrigosContent } from "./components/abrigos.js";
+// import { camisetasContent } from "./components/camisetas.js";
+// import { cardProduct } from "./components/card.js";
+// import { pantalonesContent } from "./components/pantalones.js";
 
-const containerCards = document.getElementById('container-cards');
-const navItems = document.querySelector('.sidebar__nav')
+// const containerCards = document.getElementById('container-cards');
+// const navItems = document.querySelector('.sidebar__nav')
 const counter = document.getElementById('counter')
 
-const data = [...DB.abrigo, ...DB.camiseta, ...DB.pantalon]
+// const data = [...DB.abrigo, ...DB.camiseta, ...DB.pantalon]
 
-let allProducts = data.map((e) => {
-    return cardProduct(e.imagen, e.nombre, e.precio)
-})
+// let allProducts = data.map((e) => {
+//     return cardProduct(e.imagen, e.nombre, e.precio)
+// })
 
-containerCards.innerHTML = allProducts.join('')
+// containerCards.innerHTML = allProducts.join('')
 
-navItems.addEventListener('click', (e) => {
-    const item = e.target.innerText.toLowerCase()
+// navItems.addEventListener('click', (e) => {
+//     const item = e.target.innerText.toLowerCase()
     
-    if (item === 'todos los productos') {
-        containerCards.innerHTML = allProducts.join('')
-        console.log(1);
-    }
-    if (item === 'abrigos') {
-        containerCards.innerHTML = abrigosContent
-        console.log(2);
-    }
-    if (item === 'camisetas') {
-        containerCards.innerHTML = camisetasContent
-        console.log(3);
-    }
-    if (item === 'pantalones') {
-        containerCards.innerHTML = pantalonesContent
-        console.log(4);
-    }
-})
+//     if (item === 'todos los productos') {
+//         containerCards.innerHTML = allProducts.join('')
+//         console.log(1);
+//     }
+//     if (item === 'abrigos') {
+//         containerCards.innerHTML = abrigosContent
+//         console.log(2);
+//     }
+//     if (item === 'camisetas') {
+//         containerCards.innerHTML = camisetasContent
+//         console.log(3);
+//     }
+//     if (item === 'pantalones') {
+//         containerCards.innerHTML = pantalonesContent
+//         console.log(4);
+//     }
+// })
 
 const cardBtn = document.querySelectorAll('.card__btn')
+// const car = document.getElementById('shooping-car')
 const cardBtns = [...cardBtn]
 
 let num = 0
-console.log(cardBtns);
+// const carProducts = []
 cardBtns.forEach((e) => {
     console.log(e);
     e.addEventListener('click', () => {
-        console.log('hi');
         num += 1
         counter.textContent = num
+        // AGREGAR AL CARRO
+        // carProducts.push(e.parentElement.parentElement)
+        // console.log(carProducts);
+        
     })
+    // car.addEventListener('click', (e) => {
+    //     containerCards.innerHTML = cardProduct
+    // })
 })
+
 
